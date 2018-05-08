9efb1930f37e0922d964f8e23fb1834b23152be3
DarthHamza <hamzamakia@gmail.com>
2018-05-08 13:08:48 +0300

    removed dead code

--- api/views.py
+++ api/views.py
@@ -20,8 +20,6 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 
-
-#Complete ME!
 class LoginView(APIView):
     def post(self, request):
         my_data = request.data
